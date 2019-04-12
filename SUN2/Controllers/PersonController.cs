4a169ce44826fe21a123151b359e73b5d165012b
doarnold <doar9654@gmail.com>
2019-04-12 19:48:01 +0200

    - SQL Server: Tabelle Person angelegt - Modell SUN2Model importiert - Controller PersonController generiert

---
+++ SUN2/Controllers/PersonController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SUN2.Models;
+
+namespace SUN2.Controllers
+{
+    public class PersonController : Controller
+    {
+        private SUN2Entities db = new SUN2Entities();
+
+        // GET: Person
+        public ActionResult Index()
+        {
+            var person = db.Person.Include(p => p.AspNetUsers);
+            return View(person.ToList());
+        }
+
+        // GET: Person/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.Person.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            ViewBag.id = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Person/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,vorname")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Person.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id = new SelectList(db.AspNetUsers, "Id", "Email", person.id);
+            return View(person);
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.Person.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id = new SelectList(db.AspNetUsers, "Id", "Email", person.id);
+            return View(person);
+        }
+
+        // POST: Person/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,vorname")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id = new SelectList(db.AspNetUsers, "Id", "Email", person.id);
+            return View(person);
+        }
+
+        // GET: Person/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.Person.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Person person = db.Person.Find(id);
+            db.Person.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
