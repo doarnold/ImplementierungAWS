5f1634cac0fdd55f757aa4ded602e75b501c8374
doarnold <doar9654@gmail.com>
2019-06-11 14:24:12 +0200

    Suchfunktion + Bugfix Newsfeed

--- SUN2/Controllers/HomeController.cs
+++ SUN2/Controllers/HomeController.cs
@@ -29,10 +29,29 @@ namespace SUN2.Controllers
         {
             List<NewsFeedModel> list = new List<NewsFeedModel>();
             List<Person> zuordnung = new List<Person>();
+            Boolean abonniert = false;
+            var userId = User.Identity.GetUserId();
 
             // Alle abonnierten Lehrstuhleinträge und relevanten (=mitglied) Gruppeneinträge auslesen
             foreach (LehrstuhlEintraege le in db.LehrstuhlEintraeges)
             {
+                // prüfen, ob user mitarbeiter des lehrstuhls ist oder diesen abonniert hat
+                foreach(MitarbeiterLehrstuhl ml in db.MitarbeiterLehrstuhls)
+                {
+                    if(ml.userid == userId || User.IsInRole("Admin"))
+                    {
+                        abonniert = true;
+                    }
+                }
+
+                foreach(AbonnentenLehrstuhl al in db.AbonnentenLehrstuhls)
+                {
+                    if (al.userid == userId || User.IsInRole("Admin"))
+                    {
+                        abonniert = true;
+                    }
+                }
+
                 // Userid richtig darstellen
                 foreach (Person person in db.Person)
                 {
@@ -57,20 +76,25 @@ namespace SUN2.Controllers
                     }
                 }
 
-                list.Add(new NewsFeedModel
-                {    
-                    id = le.id,
-                    entityid = le.lehrstuhlid,
-                    datum = le.datum,
-                    autor = le.autor,
-                    inhalt = le.inhalt,
-                    label1 = le.label1,
-                    label2 = le.label2,
-                    label3 = le.label3,
-                    label4 = le.label4,
-                    label5 = le.label5,
-                    typ = "l"
-                });
+                if(abonniert)
+                {
+                    list.Add(new NewsFeedModel
+                    {
+                        id = le.id,
+                        entityid = le.lehrstuhlid,
+                        datum = le.datum,
+                        autor = le.autor,
+                        inhalt = le.inhalt,
+                        label1 = le.label1,
+                        label2 = le.label2,
+                        label3 = le.label3,
+                        label4 = le.label4,
+                        label5 = le.label5,
+                        typ = "l"
+                    });
+                }
+
+                abonniert = false;
             }
 
             ////////////////////////
@@ -79,15 +103,24 @@ namespace SUN2.Controllers
 
             Boolean[] verantwortlich = new Boolean[10000]; // Index gruppenid, Eintrag false -> Gruppe gruppenid kein Verantwortlicher
             Boolean[] autor = new Boolean[10000]; // Index gruppenid, Eintrag false -> Gruppe gruppenid kein autor
+            Boolean mitglied = false;
 
             foreach (GruppenEintraege le in db.GruppenEintraeges)
             {
                 // Signal an Frontend, ob User auch autor ist und bearbeiten/löschen darf
-                var userId = User.Identity.GetUserId();
                 autor[le.id] = authCheck.AutorGE(le.gruppenid, userId);
 
                 // Signal an Frontend, ob User auch Verantwortlicher ist und somit bearbeiten/löschen darf
                 verantwortlich[le.gruppenid] = authCheck.VerantGr(le.gruppenid, userId);
+
+                // prüfen, ob user mitglied der gruppe ist
+                foreach(MitgliederGruppe mg in db.MitgliederGruppes)
+                {
+                    if(mg.userid == userId || User.IsInRole("Admin"))
+                    {
+                        mitglied = true;
+                    }
+                }
 
 
                 // Userid richtig darstellen
@@ -118,7 +151,8 @@ namespace SUN2.Controllers
                 ViewBag.autor = autor;
                 ViewBag.verantwortlich = verantwortlich;
 
- 
+            if(mitglied)
+                {
                     list.Add(new NewsFeedModel
                     {
                         id = le.id,
@@ -133,8 +167,9 @@ namespace SUN2.Controllers
                         label5 = le.label5,
                         typ = "g"
                     });
-             
-
+                   
+                }
+                mitglied = false;
 
             }
 
@@ -261,25 +296,7 @@ namespace SUN2.Controllers
 
             return View(amm);
         }
-         
-
-
-        /*
-        //liefert die about view /about
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        //liefert die contact view /contact
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        } */
+        
 
         //liefert die administrationsübersicht
         public ActionResult Administration()
@@ -287,14 +304,120 @@ namespace SUN2.Controllers
             return View();
         }
 
-        // liefert die Suchergebnisse
-        /*
+
+        // liefert die Suchergebnisse für generelle Suche auf alle DB Tabellen und Felder
+        // mit einem Suchwort als Eingabe
         public ActionResult Search(String searchstr)
         {
+            // liefert die Ergebnisse
+            List<SearchModel> sml = new List<SearchModel>();
+
+            // in gruppen suchen
+            var gruppe = db.Gruppes.Where(p => p.bezeichnung.Contains(searchstr) || 
+                                   p.beschreibung.Contains(searchstr) ||
+                                   p.verantwortlicher.Contains(searchstr));
+
+            foreach(Gruppe gr in gruppe)
+            {
+                sml.Add(new SearchModel
+                {
+                    id = 0,
+                    entityid = gr.gruppenid,
+                    field1 = gr.bezeichnung,
+                    field2 = gr.verantwortlicher,
+                    field3 = "",
+                    field4 = "",
+                    typ = "g"
+                });
+            }
+
+            // in lehrstühlen suchen
+            var lehrstuhl = db.Lehrstuhls.Where(p => p.bezeichnung.Contains(searchstr) ||
+                                   p.beschreibung.Contains(searchstr) ||
+                                   p.verantwortlicher.Contains(searchstr));
+
+            foreach (Lehrstuhl l in lehrstuhl)
+            {
+                sml.Add(new SearchModel
+                {
+                    id = 0,
+                    entityid = l.lehrstuhlid,
+                    field1 = l.bezeichnung,
+                    field2 = l.verantwortlicher,
+                    field3 = "",
+                    field4 = "",
+                    typ = "l"
+                });
+            }
+
+            // in gruppeneinträgen suchen
+            var gruppeneintraege = db.GruppenEintraeges.Where(p => p.autor.Contains(searchstr) ||
+                                   p.inhalt.Contains(searchstr) ||
+                                   p.label1.Contains(searchstr) ||
+                                   p.label2.Contains(searchstr) ||
+                                   p.label3.Contains(searchstr) ||
+                                   p.label4.Contains(searchstr) ||
+                                   p.label5.Contains(searchstr));
+
+            foreach (GruppenEintraege ge in gruppeneintraege)
+            {
+                sml.Add(new SearchModel
+                {
+                    id = ge.id,
+                    entityid = ge.gruppenid,
+                    field1 = ge.inhalt,
+                    field2 = ge.autor,
+                    field3 = ge.datum.ToString(),
+                    field4 = "",
+                    typ = "ge"
+                });
+            }
 
 
+            // in lehrstuhleinträgen suchen
+            var lehrstuhleintraege = db.LehrstuhlEintraeges.Where(p => p.autor.Contains(searchstr) ||
+                                   p.inhalt.Contains(searchstr) ||
+                                   p.label1.Contains(searchstr) ||
+                                   p.label2.Contains(searchstr) ||
+                                   p.label3.Contains(searchstr) ||
+                                   p.label4.Contains(searchstr) ||
+                                   p.label5.Contains(searchstr));
+
+            foreach (LehrstuhlEintraege le in lehrstuhleintraege)
+            {
+                sml.Add(new SearchModel
+                {
+                    id = le.id,
+                    entityid = le.lehrstuhlid,
+                    field1 = le.inhalt,
+                    field2 = le.autor,
+                    field3 = le.datum.ToString(),
+                    field4 = "",
+                    typ = "le"
+                });
+            }
+
+
+            // in personen suchen
+            var personen = db.Person.Where(p => p.name.Contains(searchstr) ||
+                                   p.vorname.Contains(searchstr));
+
+            foreach (Person p in personen)
+            {
+                sml.Add(new SearchModel
+                {
+                    id = 0,
+                    entityid = 0,
+                    field1 = p.vorname + p.name + "(" + p.AspNetUsers.Email + ")",
+                    field2 = p.AspNetUsers.Role,
+                    field3 = p.AspNetUsers.Email,
+                    field4 = "",
+                    typ = "p"
+                });
+            }
+
+            return View(sml);
         }
-        */
 
 
     }
---
+++ SUN2/Models/SearchModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SUN2.Models
+{
+
+    public class SearchModel
+    {
+        public int id { get; set; } // zb gruppeneintragsID
+        public int entityid { get; set; } //lehrstuhlid oder gruppenid
+
+        public string field1 { get; set; }
+        public string field2 { get; set; }
+        public string field3 { get; set; }
+        public string field4 { get; set; }
+        public string typ { get; set; }  //g = gruppe, ge = gruppeneinträge, l = lehrstuhl, le = lehrstuhleinträge, p = person
+
+    }
+}
