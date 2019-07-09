5c44a93cf3a16b8ee02a1da72c912a1553020b92
doarnold <doar9654@gmail.com>
2019-07-09 16:36:11 +0200

    kennwort ändern added

--- SUN2/Controllers/ManageController.cs
+++ SUN2/Controllers/ManageController.cs
@@ -245,7 +245,7 @@ namespace SUN2.Controllers
                 {
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
                 }
-                return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
+                return RedirectToAction("Index","Home");
             }
             AddErrors(result);
             return View(model);
